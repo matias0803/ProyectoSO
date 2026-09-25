fedc65636caf674d2848084305df38f985c63a05
matias0803 <matias08031@hotmail.com>
2026-09-24 23:14:24 -0300

    cambio en los include

--- user/find.c
+++ user/find.c
@@ -1,59 +1,62 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/fs.h"
+#define DIRSIZ 14
 
-void buscarArchivo(char* inicio, char* nombre){
+struct dirent {
+    ushort inum;
+    char name[DIRSIZ];
+};
+
+void buscarArchivo(char *inicio, char *nombre) {
+    char buf[512];
+    char *p;
     int fd;
     struct dirent de;
     struct stat st;
-    char buf[512];
-    char *p;
 
-   
-    fd = open(inicio, 0); 
-    if (fd < 0) {
+    if ((fd = open(inicio, 0)) < 0) {
         return;
     }
 
-    
     if (fstat(fd, &st) < 0) {
         close(fd);
         return;
     }
 
-    
-    if (st.type == T_DIR) {
-        
-        strcpy(buf, inicio);
-        p = buf + strlen(buf);
-        *p++ = '/'; 
+    if (st.type != T_DIR) {
+        close(fd);
+        return;
+    }
 
-        
-        while(read(fd, &de, sizeof(de)) == sizeof(de)){
-            
-            
-            if(de.inum == 0) continue;
+    if (strlen(inicio) + 1 + DIRSIZ + 1 > sizeof(buf)) {
+        close(fd);
+        return;
+    }
 
-            
-            memmove(p, de.name, DIRSIZ);
-            p[DIRSIZ] = 0;
+    strcpy(buf, inicio);
+    p = buf + strlen(buf);
+    *p++ = '/';
 
-            
-            if (strcmp(nombre, de.name) == 0) {
-                printf("%s\n", buf);
-            }
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+        if (de.inum == 0)
+            continue;
 
-            
-            if (stat(buf, &st) < 0) {
-                continue;
-            }
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
 
-            
-            if (st.type == T_DIR && strcmp(de.name, ".") != 0 && strcmp(de.name, "..") != 0) {
-                
-                buscarArchivo(buf, nombre);
-            }
+        if (strcmp(p, ".") == 0 || strcmp(p, "..") == 0)
+            continue;
+
+        if (stat(buf, &st) < 0)
+            continue;
+
+        if (strcmp(p, nombre) == 0) {
+            printf("%s\n", buf);
+        }
+
+        if (st.type == T_DIR) {
+            buscarArchivo(buf, nombre);
         }
     }
 
@@ -61,10 +64,11 @@ void buscarArchivo(char* inicio, char* nombre){
 }
 
 int main(int argc, char *argv[]) {
-  if(argc != 3){
-    fprintf(2, "Uso: find <ruta_inicial> <nombre_archivo>\n");
-    exit(1);
-  }
-  buscarArchivo(argv[1], argv[2]);
-  exit(0);
+    if (argc != 3) {
+        fprintf(2, "Uso: find <directorio> <nombre_archivo>\n");
+        exit(1);
+    }
+
+    buscarArchivo(argv[1], argv[2]);
+    exit(0);
 }
